505cabc25fdde03a83f49cfef68fbd4f2aa54abb
Roberto Sánchez F. <63380708+RobertoASF@users.noreply.github.com>
2026-07-13 23:18:56 -0400

    Ampliar modelo de juegos para Firebase

--- src/app/models/juego.model.ts
+++ src/app/models/juego.model.ts
@@ -1,19 +1,21 @@
 export interface Juego {
-    nombre: string;
-    descripcion: string;
-    precio: number;
-    descuento: boolean;
-    descuentoTexto: string;
-    porcentajeDescuento?: number;
-    imagen: string;
-    chip: string;
-  }
-  
-  export interface Categoria {
-    slug: string;
-    nombre: string;
-    icono: string;
-    descripcion: string;
-    imagen: string;
-    juegos: Juego[];
-  }
+  id?: string;
+  categoriaSlug: string;
+  nombre: string;
+  descripcion: string;
+  precio: number;
+  descuento: boolean;
+  descuentoTexto: string;
+  porcentajeDescuento?: number;
+  imagen: string;
+  chip: string;
+}
+
+export interface Categoria {
+  slug: string;
+  nombre: string;
+  icono: string;
+  descripcion: string;
+  imagen: string;
+  juegos?: Juego[];
+}
