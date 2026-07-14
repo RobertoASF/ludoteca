0c4d309a09fff09e2af17311ffda2b12ea8c07cf
Roberto Sánchez F. <63380708+RobertoASF@users.noreply.github.com>
2026-07-13 23:20:24 -0400

    Agregar ruta admin para CRUD de juegos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Registro } from './pages/registro/registro';
 import { Perfil } from './pages/perfil/perfil';
 import { Recuperar } from './pages/recuperar/recuperar';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { AdminJuegos } from './pages/admin-juegos/admin-juegos';
 
 import { authGuard  } from './guards/auth-guard';
 import { adminGuard } from './guards/admin-guard';
@@ -16,5 +17,6 @@ export const routes: Routes = [
   { path: 'perfil', component: Perfil, canActivate: [authGuard] },
   { path: 'recuperar', component: Recuperar },
   { path: 'admin/dashboard', component: Dashboard, canActivate: [adminGuard] },
+  { path: 'admin/juegos', component: AdminJuegos, canActivate: [adminGuard] },
   { path: '**', redirectTo: '' }
 ];
